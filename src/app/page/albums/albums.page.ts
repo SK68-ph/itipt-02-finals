ab356ea96c05f6204a95fae80434bfb19282980b
MMS <35738176+SK68-ph@users.noreply.github.com>
2022-11-30 19:25:29 +0800

    update albums page

--- src/app/page/albums/albums.page.ts
+++ src/app/page/albums/albums.page.ts
@@ -6,7 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./albums.page.scss'],
 })
 export class AlbumsPage implements OnInit {
+  isModalOpen = false;
 
+  setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
+  }
+  isModalOpen2 = false;
+
+  setOpen2(isOpen: boolean) {
+    this.isModalOpen2 = isOpen;
+  }
+  isModalOpen3 = false;
+
+  setOpen3(isOpen: boolean) {
+    this.isModalOpen3 = isOpen;
+  }
+  isModalOpen4 = false;
+
+  setOpen4(isOpen: boolean) {
+    this.isModalOpen4 = isOpen;
+  }
+  isModalOpen5 = false;
+
+  setOpen5(isOpen: boolean) {
+    this.isModalOpen5 = isOpen;
+  }
   constructor() { }
 
   ngOnInit() {
